95897715bc9dbafd3fe5d64875d496d2a74f7c94
Anoop Jadhav <anojadhav@deloitte.com>
2020-11-28 03:52:26 +0530

    first commit

--- index.js
+++ index.js
@@ -26,22 +26,9 @@ client.debug = function (msg) {
 }
 
 
-// let tableData = [];
-// function connectCallback(result) {
-//   // document.getElementById('stomp-status').innerHTML = "It has now successfully connected to a stomp server serving price updates for some foreign exchange currency pairs."
-//   client.subscribe('/fx/prices', function (message) {
-//     console.log(JSON.parse(message.body));
-//   });
-// }
-
-
 client.connect({}, connectCallback, function (error) {
   alert(error.headers.message)
 })
-
-// const exampleSparkline = document.getElementById('example-sparkline')
-// Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3])
-
 
 
 class TableClass {
@@ -50,33 +37,95 @@ class TableClass {
     this.currencies = [];//Object of rowId & currency
     this.tableData = [];
   }
-  // currencies = [];
-  // tableData = []; 
+
   addCurrency(currency) {
     this.currencies.push({
       'rowId': this.tableData.length,
       'currency': currency
     });
-
     return this.tableData.length;
   }
 
-  renderRow(rowData, rowId) {
+  renderTable() {
+    this.tableData.sort((a, b) => {
+      if (parseFloat(a.lastChangeBid) < parseFloat(b.lastChangeBid)) {
+        return 1;
+      } else {
+        return -1
+      }
+    })
+
+    let tBody = document.querySelector('table > tbody');
+    while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+    }
+    this.tableData.forEach((rowData) => {
+      var row = document.createElement('tr');
+      row.id = rowData.id;
+
+      let rowItem;
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'name';
+      rowItem.innerText = rowData.name.toString();
+      row.appendChild(rowItem);
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'bestBid';
+      rowItem.innerText = rowData.bestBid.toString();
+      row.appendChild(rowItem);
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'bestAsk';
+      rowItem.innerText = rowData.bestAsk.toString();
+      row.appendChild(rowItem);
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'lastChangeAsk';
+      rowItem.innerText = rowData.lastChangeAsk.toString();
+      row.appendChild(rowItem);
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'lastChangeBid';
+      rowItem.innerText = rowData.lastChangeBid.toString();
+      row.appendChild(rowItem);
+
+      tBody.appendChild(row);
+    })
+  }
+
+  renderRow(rowData) {
     try {
       let tBody = document.querySelector('table > tbody');
       var row = document.createElement('tr');
-      row.id = rowId;
-      // console.log(Object.keys(rowData));
-      Object.keys(rowData).forEach((rowItemKey) => {
-        if (rowItemKey) {
-          if (rowItemKey === 'name' || rowItemKey === 'bestBid' || rowItemKey === 'bestAsk' || rowItemKey === 'lastChangeAsk' || rowItemKey === 'lastChangeBid') {
-            let rowItem = document.createElement('td');
-            rowItem.id = rowItemKey;
-            rowItem.innerText = rowData[rowItemKey];
-            row.appendChild(rowItem);
-          }
-        }
-      })
+      row.id = rowData.id;
+
+      let rowItem;
+      rowItem = document.createElement('td');
+      rowItem.id = 'name';
+      rowItem.innerText = rowData.name.toString();
+      row.appendChild(rowItem);
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'bestBid';
+      rowItem.innerText = rowData.bestBid.toString();
+      row.appendChild(rowItem);
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'bestAsk';
+      rowItem.innerText = rowData.bestAsk.toString();
+      row.appendChild(rowItem);
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'lastChangeAsk';
+      rowItem.innerText = rowData.lastChangeAsk.toString();
+      row.appendChild(rowItem);
+
+      rowItem = document.createElement('td');
+      rowItem.id = 'lastChangeBid';
+      rowItem.innerText = rowData.lastChangeBid.toString();
+      row.appendChild(rowItem);
+
       tBody.appendChild(row);
 
     } catch (e) {
@@ -84,13 +133,73 @@ class TableClass {
     }
   }
 
-  createNewRow(rowData) {
+  pushTableRowAtIndex(index, currentRowData) {
+    // index = 4 currentRowIndex=2
+    let currentRowIndex;
+    let localTableData = JSON.parse(JSON.stringify(this.tableData));
+
+    for (let i = 0; i < localTableData.length; i++) {
+      if (localTableData[i].id === currentRowData.id)
+        currentRowIndex = i;
+    }
+
+    localTableData.splice(index, 0, currentRowData);
+
+    if (index >= currentRowIndex) {
+      //index remains same even after slicing
+      localTableData.splice(currentRowIndex, 1);
+    } else {
+      //index changes by one
+      localTableData.splice(currentRowIndex + 1, 1);
+    }
+    //update currentRow to its new index 
+    this.tableData = localTableData;
+
+  }
+
+  pushIndex(currentRowData) {
+    try {
+      //calculate the index where the new element needs to be pushed
+      if (this.tableData.length > 1) {
+        let tBody = document.querySelector('table > tbody');
+
+        for (let i = 0; i < this.tableData.length; i++) {
+          if (this.tableData[i].id != currentRowData.id && (parseFloat(this.tableData[i].lastChangeBid) < parseFloat(currentRowData.lastChangeBid)) || this.tableData.length === i) {
+            if (this.tableData.length !== i) {
+              //push it to its new index            
+              let currentRowEle = document.querySelector(`tr[id='${currentRowData.id}']`);
+              let pushBeforeRowEle = document.querySelector(`tr[id='${this.tableData[i].id}']`);
+
+              tBody.insertBefore(currentRowEle, pushBeforeRowEle);
+              // //change the table order as well
+
+              this.pushTableRowAtIndex(i, currentRowData);
+            } else {
+              //push the element at the last 
+              let currentRowEle = document.querySelector(`tr[id='${currentRowData.id}']`);
+              tBody.appendChild(currentRowEle);
+              // //change the table order as well
+              this.pushTableRowAtIndex(i, currentRowData);
+            }
+            break;
+          }
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+  }
+
+  createNewRow(currentRowData) {
     try {
       //push the new row
-      let rowId = this.addCurrency(rowData.name);
-      rowData.id = rowId;
-      this.tableData.push(rowData);
-      this.renderRow(rowData, rowId);
+      let rowId = this.addCurrency(currentRowData.name);
+      currentRowData.id = rowId;
+      this.tableData.push(currentRowData);
+      // this.renderRow(currentRowData);
+      // this.pushIndex(currentRowData);
+
     } catch (e) {
       console.log(e);
     }
@@ -105,28 +214,45 @@ class TableClass {
     return rowId;
   }
 
-  highlightItem(tRow){
+  highlightItem(tRow) {
     tRow.classList.add('blink');
-    setTimeout(function(){
+    setTimeout(function () {
       tRow.classList.remove('blink');
-    },2000);
+    }, 500);
   }
 
-  updateRowItem(rowItem,value){
-   rowItem.innerText = value;
-   this.highlightItem(rowItem);
+  updateRowItem(rowItem, value) {
+    rowItem.innerText = value;
+    this.highlightItem(rowItem);
   }
-  
+
+  updateTableRowData(newRowData) {
+    let updateRowId;
+    for (let i = 0; i < this.tableData.length; i++) {
+      if (this.tableData[i].name === newRowData.name) {
+        this.tableData[i].bestBid = newRowData.bestBid;
+        this.tableData[i].bestAsk = newRowData.bestAsk;
+        this.tableData[i].lastChangeAsk = newRowData.lastChangeAsk;
+        this.tableData[i].lastChangeBid = newRowData.lastChangeBid;
+        updateRowId = this.tableData[i].id;
+        break;
+      }
+    }
+    return this.tableData[updateRowId];
+  }
+
   updateExistingRow(rowData) {
-    let rowId = this.getRowId(rowData.name)
-    let tRow = document.querySelector(`tr[id='${rowId}']`);
-    if(tRow){
-      this.updateRowItem(tRow.querySelector('#name'),rowData.name);
-      this.updateRowItem(tRow.querySelector('#bestBid'),rowData.bestBid);
-      this.updateRowItem(tRow.querySelector('#bestAsk'),rowData.bestAsk);
-      this.updateRowItem(tRow.querySelector('#lastChangeAsk'),rowData.lastChangeAsk);
-      this.updateRowItem(tRow.querySelector('#lastChangeBid'),rowData.lastChangeBid);
-    }    
+    rowData = this.updateTableRowData(rowData);
+    let tRow = document.querySelector(`tr[id='${rowData.id}']`);
+    if (tRow) {
+      this.updateRowItem(tRow.querySelector('#name'), rowData.name);
+      this.updateRowItem(tRow.querySelector('#bestBid'), rowData.bestBid);
+      this.updateRowItem(tRow.querySelector('#bestAsk'), rowData.bestAsk);
+      this.updateRowItem(tRow.querySelector('#lastChangeAsk'), rowData.lastChangeAsk);
+      this.updateRowItem(tRow.querySelector('#lastChangeBid'), rowData.lastChangeBid);
+    }
+    // this.pushIndex(rowData);
+
   }
 
   checkIfAlreadyPresent(key) {
@@ -144,6 +270,15 @@ class TableClass {
   }
 
 
+  generateSparkLines(){
+    try{
+    const exampleSparkline = document.getElementById('chart')
+    Sparkline.draw(exampleSparkline, [1, 2, 3, 6, 8, 20, 2, 2, 4, 2, 3])
+    }catch(e){
+      console.log(e);
+    }
+  }
+
 }
 
 const tableObj = new TableClass();
@@ -151,20 +286,19 @@ const tableObj = new TableClass();
 function connectCallback(result) {
   client.subscribe('/fx/prices', function (message) {
     if (message.body) {
-
       let result = JSON.parse(message.body);
-
       let isalreadyPresent = tableObj.checkIfAlreadyPresent(result.name)
-      // console.log(tableObj.currencies)
       if (isalreadyPresent) {
         //update existing entry 
         tableObj.updateExistingRow(result);
-        console.log('already exists');
       } else {
         //unqiue add an entry
         //push it in the currencies array
         tableObj.createNewRow(result);
       }
+
+      tableObj.renderTable();
+      tableObj.generateSparkLines();
     }
   });
 }
